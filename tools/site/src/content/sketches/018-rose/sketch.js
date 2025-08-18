95a25bead4fe7484ef55b09c3791ee82ee944d72
Christopher Bellanger <cricrio@users.noreply.github.com>
2025-08-18 22:05:15 +0200

    improve 018 roses

--- tools/site/src/content/sketches/018-rose/sketch.js
+++ tools/site/src/content/sketches/018-rose/sketch.js
@@ -1,5 +1,6 @@
 const colors = ['#ffe5ec', '#ffc2d1', '#ffb3c6', '#ff8fab', '#fb6f92'];
 let size = 150;
+let direction = -1;
 
 function setup() {
   frameRate(16);
@@ -8,15 +9,29 @@ function setup() {
 }
 
 function draw() {
-  if (size) {
-    push();
-    translate(width / 2, height / 2);
-    rotate((frameCount * 15) % 360);
+  push();
+  translate(width / 2, height / 2);
+  rotate((frameCount * 15) % 360);
+
+  if (direction > 0) {
+    noStroke();
+    fill('white');
+  } else {
     fill(random(colors));
-    square(0, 0, size);
-    pop();
-    size = size - 1;
   }
+  square(0, 0, size);
+  pop();
+
+  size = size + direction;
+
+  if (size < 0) {
+    direction = 1;
+  }
+  if (size > 170) {
+    direction = -1;
+    size = 150;
+  }
+  console.log(size);
 }
 
 function reset() {
